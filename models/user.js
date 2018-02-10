a1350d547e9ef04afc9a49e357a14b14ead2fdf5
mayurxy <mayurxy08@gmail.com>
2018-02-10 10:27:05 -0800

    user model created

---
+++ models/user.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose');
+var bcrypt  =   require('bcrypt-nodejs');
+var Schema = mongoose.Schema;
+
+
+var userSchema  =   new Schema({
+    email:      {type:String, required:true},
+    password:   {type:String, required:true},
+});
+
+module.exports  =   mongoose.model('User',userSchema);
