fe735075bb7376d39e072af5a321b7c2c89577e4
mayurxy <mayurxy08@gmail.com>
2018-02-10 23:58:22 -0800

    csurf token added for middleware

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var expressHbs = require('express-handlebars');
 var mongoose = require('mongoose');
+var session = require('express-session');
 
 var routes = require('./routes/index');
 
@@ -23,7 +24,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(session({secret : 'mysupersecret', resave: false, saveUninitialized: false}));
 app.use('/', routes);
 
 // catch 404 and forward to error handler
