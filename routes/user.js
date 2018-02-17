e9e07805ad2fd2e806b9e31b38dc189e8ea66b34
mayurxy <mayurxy08@gmail.com>
2018-02-17 09:36:23 -0800

    final touches completed

--- routes/user.js
+++ routes/user.js
@@ -2,11 +2,23 @@ var express = require('express');
 var router = express.Router();
 var csrf = require('csurf');
 var passport = require('passport');
-
+var Cart = require('../models/cart');
 var csrfProtection = csrf();
 router.use(csrfProtection);
 
 router.get('/profile', isLoggedIn, function (req, res, next) {
+    Order.find({user:   req.user}, function(err, orders){
+        if(err){
+            return res.write('Error!');
+        }
+        var cart;
+        orders.forEach(function(order){
+            cart = new Cart(order.cart);
+            order.items = cart.generateArray();
+        });
+        res.render('user/profile',{orders:orders});
+
+    });
     res.render('user/profile');
 });
 
@@ -36,10 +48,19 @@ router.get('/signin', function (req, res, next) {
 });
 
 router.post('/signin', passport.authenticate('local.signin', {
-    successRedirect: '/user/profile',
+    //successRedirect: '/user/profile',
     failureRedirect: '/user/signin',
     failureFlash: true
-}));
+}), function(req, res, next){
+    if(req.session.oldUrl){
+        redirect(req.session.oldUrl);
+        req.session.oldUrl = null;
+
+    }else{
+        res.redirect('/user/profile');
+        
+    }
+});
 
 module.exports = router;
 
